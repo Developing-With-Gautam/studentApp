0734f529d7dc2e15370d5451e447cf464582e7e4
Gautam Yadav <gautam.yadav@nexxbase.com>
2025-09-29 21:03:02 +0530

    student data added in the database

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from database import get_db,Base,engine
+from sqlalchemy.orm import Session
+from scheme import student
+from models import studentModel
+
 
 app= FastAPI()
+Base.metadata.create_all(engine)
+
 
 @app.get('/')
 def homepage():
     return "Student check in website"
+
+@app.post('/students')
+def add_student(data:student.Students_data , db:Session = Depends(get_db)):
+
+    new_student = studentModel(name = data.name,email = data.email,student_id= data.student_id)
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+    return new_student
+    
+
--- models/studentModel.py
+++ models/studentModel.py
@@ -1,17 +1,24 @@
 from database import Base
 from sqlalchemy import Integer,String,Column,DateTime
-from sqlalchemy.orm import 
+from sqlalchemy.orm import relationship
 
 
-class Student_table(Base):
+class StudentUserModel(Base):
     __tablename__ ="student"
     id = Column(Integer,autoincrement=True,index=True,primary_key=True)
     name = Column(String,nullable=False)
     email = Column(String,nullable=False,unique=True)
+    student_id = Column(Integer,nullable= False ,unique= True)
+    checkIn = relationship("CheckinStudentModel",back_populates="student")
 
-class Checkin_table(Base):
+class CheckinStudentModel(Base):
     __tablename__= "student_checkin"
     id=Column(Integer,primary_key=True,index=True)
+    studentId_fk = Column(Integer,foreign_key = "student_id")
     timestamp = Column(DateTime,default=DateTime.utcnow)
+    student = relationship("StudentUserModel",back_populates="checkIn")
+
+
+
 
 
