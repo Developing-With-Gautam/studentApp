73e9467e04a0a81514cb8a2af3ae86b8bb620eca
Gautam Yadav <gautam.yadav@nexxbase.com>
2025-09-30 15:26:20 +0530

    backend deploy

--- database.py
+++ database.py
@@ -1,18 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/student")
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/student"
-
-engine  = create_engine(DATABASE_URL)
-
-sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = sessionLocal()
-
+    db = SessionLocal()
     try:
         yield db
     finally:
